ca788f4e33a05171427307013f055d2b9840ddb7
sumukhahm2 <137076103+sumukhahm2@users.noreply.github.com>
2023-07-13 10:02:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,97 +1,194 @@
+//axios globals
+axios.defaults.headers.common['X-auth-token']='sometoken';
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
-}
+    axios('https://jsonplaceholder.typicode.com/todos?_limit=5'
+    ).then((res)=>showOutput(res))
+    .catch((err)=> console.log(err))
+  }
+  
+  // POST REQUEST
+  function addTodo() {
+    axios({
+        method: 'post',
+        url:'https://jsonplaceholder.typicode.com/todos?_limit=5',
+        data:{
+            title:'new_todo',
+            completed:false
+        }
+    }).then((res)=>showOutput(res))
+    .catch((err)=> console.log(err))
+  }
+  
+  // PUT/PATCH REQUEST
+  function updateTodo() {
+    axios.patch('https://jsonplaceholder.typicode.com/todos/1',{
+        title:'new_todo',
+        completed:false
+    })
+    .then((res)=>showOutput(res))
+    .catch((err)=> console.log(err))
+  }
+  
+  // DELETE REQUEST
+  function removeTodo() {
+    axios.delete('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res)=>showOutput(res))
+    .catch((err)=> console.log(err))
+  }
+  
+  // SIMULTANEOUS DATA
+  function getData() {
+    axios.all([
+        axios.get('https://jsonplaceholder.typicode.com/todos'),
+        axios.post('https://jsonplaceholder.typicode.com/posts')
+    ]).then(axios.spread((todos,posts)=> showOutput(posts)))
+    .catch(err=> console.error(err))
+  }
+  
+  // CUSTOM HEADERS
+  function customHeaders() {
+    const config={
+        headers:{
+            'Content-Type':'application/json',
+        Authorization:'sometoken'
+        }
+    };
+    axios.post('https://jsonplaceholder.typicode.com/todos',
+        {
+            title:'new_todo',
+            completed:false
+        }
+    ,config).then((res)=>showOutput(res))
+    .catch((err)=> console.log(err))
+  }
+  
+  // TRANSFORMING REQUESTS & RESPONSES
+  function transformResponse() {
+    const options={
+        method:'post',
+        url:'https://jsonplaceholder.typicode.com/todos',
+        data:{
+            title:'hello world'
+        },
+        transformResponse:axios.defaults.transformResponse.concat(data=>{
+            data.title=data.title.toUpperCase();
+            return data;
+        })
+    }
+    axios(options).then(res=> showOutput(res))
+  }
+  
+  // ERROR HANDLING
+  function errorHandling() {
+    axios.get('https://jsonplaceholder.typicode.com/todoss',{
+        validateStatus:function(status){
+            return status<500;
+        }
+    })
 
-// POST REQUEST
-function addTodo() {
-  console.log('POST Request');
-}
+    .then((res)=>showOutput(res))
+    .catch(err=>{
+        if(err.response)
+        {
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        }
+        if(err.response.status===404)
+        {
+            alert('error:page not found')
+        }
+        else if(err.request)
+        {
+            console.error(err.request)
+        }
+        else 
+        {
+            console.error(err.message)
+        }
+    })
+  }
+  
+  // CANCEL TOKEN
+  function cancelToken() {
+    const source=axios.CancelToken.source();
+    axios.get('https://jsonplaceholder.typicode.com/todos',{
+        cancelToken:source.token
+    })
+    .then((res)=>showOutput(res))
+    .catch(thrown=>{
+        if(axios.isCancel(thrown))
+        {
+            console.log('request canceled',thrown.message)
+        }
+    })
+    if(true)
+    {
+        source.cancel('request canceled')
+    }
+  }
+  
+  // INTERCEPTING REQUESTS & RESPONSES
+  axios.interceptors.request.use(config=>{
+    console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`)
+    return  config;
 
-// PUT/PATCH REQUEST
-function updateTodo() {
-  console.log('PUT/PATCH Request');
-}
-
-// DELETE REQUEST
-function removeTodo() {
-  console.log('DELETE Request');
-}
-
-// SIMULTANEOUS DATA
-function getData() {
-  console.log('Simultaneous Request');
-}
-
-// CUSTOM HEADERS
-function customHeaders() {
-  console.log('Custom Headers');
-}
-
-// TRANSFORMING REQUESTS & RESPONSES
-function transformResponse() {
-  console.log('Transform Response');
-}
-
-// ERROR HANDLING
-function errorHandling() {
-  console.log('Error Handling');
-}
-
-// CANCEL TOKEN
-function cancelToken() {
-  console.log('Cancel Token');
-}
-
-// INTERCEPTING REQUESTS & RESPONSES
-
-// AXIOS INSTANCES
-
-// Show output in browser
-function showOutput(res) {
-  document.getElementById('res').innerHTML = `
-  <div class="card card-body mb-4">
-    <h5>Status: ${res.status}</h5>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Headers
+  },error=>{
+    return Promise.reject(error)
+  })
+  
+  // AXIOS INSTANCES
+  const axiosInstance=axios.create({
+    baseURL:'https://jsonplaceholder.typicode.com'
+  });
+  axiosInstance.get('/comments').then((res)=> showOutput(res))
+  // Show output in browser
+  function showOutput(res) {
+    document.getElementById('res').innerHTML = `
+    <div class="card card-body mb-4">
+      <h5>Status: ${res.status}</h5>
     </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Headers
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+      </div>
     </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Data
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Data
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.data, null, 2)}</pre>
+      </div>
     </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Config
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.config, null, 2)}</pre>
+      </div>
     </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Config
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.config, null, 2)}</pre>
-    </div>
-  </div>
-`;
-}
-
-// Event listeners
-document.getElementById('get').addEventListener('click', getTodos);
-document.getElementById('post').addEventListener('click', addTodo);
-document.getElementById('update').addEventListener('click', updateTodo);
-document.getElementById('delete').addEventListener('click', removeTodo);
-document.getElementById('sim').addEventListener('click', getData);
-document.getElementById('headers').addEventListener('click', customHeaders);
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
-document.getElementById('error').addEventListener('click', errorHandling);
-document.getElementById('cancel').addEventListener('click', cancelToken);
+  `;
+  }
+  
+  // Event listeners
+  document.getElementById('get').addEventListener('click', getTodos);
+  document.getElementById('post').addEventListener('click', addTodo);
+  document.getElementById('update').addEventListener('click', updateTodo);
+  document.getElementById('delete').addEventListener('click', removeTodo);
+  document.getElementById('sim').addEventListener('click', getData);
+  document.getElementById('headers').addEventListener('click', customHeaders);
+  document
+    .getElementById('transform')
+    .addEventListener('click', transformResponse);
+  document.getElementById('error').addEventListener('click', errorHandling);
+  document.getElementById('cancel').addEventListener('click', cancelToken);
